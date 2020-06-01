02ada2a2089bac9ea518bc03c587a6ce074838e8
victorromeo <ian.thomas.west@gmail.com>
2020-06-01 12:51:20 +1000

    Simple Histograms for stats

--- ml_utilities/deepsqueak_utils.py
+++ ml_utilities/deepsqueak_utils.py
@@ -1,5 +1,6 @@
 import mat73, h5py, os
 from ml_utilities import Audio
+import matplotlib.pyplot as plt
 import tqdm.auto as tqdm
 
 def read_matlab(mat_path):
@@ -92,6 +93,7 @@ class DeepSqueak():
         return self.get_audio().get_sample_box(start_time, min_freq* 1000, width_time, height_freq * 1000)
   
 def bulk_analysis(detect_folder, audio_folder):
+    ''' Calculates basic quality and boundary arrays '''
     remove_wav = lambda x : x.replace('.wav','').replace('.WAV','')
     remove_mat = lambda x : x.replace('.mat','')
 
@@ -126,3 +128,59 @@ def bulk_analysis(detect_folder, audio_folder):
     
     return joined, (all_boxes, all_scores, all_power, all_accepts)
     
+def plot_call_quality(all_boxes, all_scores, all_power, all_accepts):
+    ''' Plots histograms for Score, Power and Accept stats '''
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    fig.set_figheight(4)
+    fig.set_figwidth(15)
+
+    ax1.hist(all_scores, color='lightblue', edgecolor='black', bins=int(180/5))
+    ax1.set_title('Histogram of Scores')
+    ax1.set_xlabel('Score')
+    ax1.set_ylabel('Detections')
+
+    ax2.hist(all_power, color='lightblue', edgecolor='black', bins=int(180/5))
+    ax2.set_title('Histogram of Scores')
+    ax2.set_xlabel('Power')
+    ax2.set_ylabel('Detections')
+
+    ax3.hist(all_accepts, color='lightblue', edgecolor='black', bins=int(180/5))
+    ax3.set_title('Histogram of Accept')
+    ax3.set_xlabel('Accept int(bool)')
+    ax3.set_ylabel('Detections')
+    
+def plot_call_boundary(all_boxes, all_scores, all_power, all_accepts):
+    ''' Plots histograms for the call boundary boxes '''
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+    fig.set_figheight(10)
+    fig.set_figwidth(14)
+
+    ax1.hist([b[0] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax1.set_title('Call Start Time')
+    ax1.set_xlabel('Offset (seconds) from clip start')
+    ax1.set_ylabel('Detections')
+
+    ax2.hist([b[0] + b[2] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax2.set_title('Call End Time')
+    ax2.set_xlabel('Duration')
+    ax2.set_ylabel('Detections')
+
+    ax3.hist([b[2] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax3.set_title('Call Length')
+    ax3.set_xlabel('Duration')
+    ax3.set_ylabel('Detections')
+
+    ax4.hist([b[1] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax4.set_title('Call Low Frequency')
+    ax4.set_xlabel('Frequency (kHz)')
+    ax4.set_ylabel('Detections')
+
+    ax5.hist([b[1] + b[3] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax5.set_title('Call High Frequency')
+    ax5.set_xlabel('Frequency (kHz)')
+    ax5.set_ylabel('Detections')
+
+    ax6.hist([b[3] for b in all_boxes], color='lightblue', edgecolor='black', bins=int(180/5))
+    ax6.set_title('Call Height')
+    ax6.set_xlabel('Frequency (kHz)')
+    ax6.set_ylabel('Detections')
